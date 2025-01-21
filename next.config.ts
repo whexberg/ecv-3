90f2f273c51631994fbac75441a8e8b53f3a670f
William Hexberg <whexberg@aadyasecurity.com>
2025-01-20 18:06:54 -0800

    Update basePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    basePath: 'https://whexberg.github.io/ecv-3/',
     output: 'export',
     images: {
         unoptimized: true,
