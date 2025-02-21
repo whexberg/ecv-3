a896668c014bcaeaa02c2bca350efb685b11277c
William Hexberg <whexberg@aadyasecurity.com>
2025-02-20 20:43:27 -0800

    Replace bean feed info with spring doins

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
+import React from 'react';
 
-import { Heading, Paragraph } from '@/components/heading';
+import { Heading, Paragraph, Subheading } from '@/components/heading';
 import PageSection from '@/components/page-section';
 
 const sponsors = [
@@ -50,77 +51,114 @@ export default function Home() {
                 />
             </PageSection>
 
-            <PageSection heading="Upcoming Events" id="beanfeed">
+            <PageSection heading="Upcoming Events" id="events">
                 <div className="flex flex-col lg:flex-row gap-8 items-center justify-center">
                     <div className="basis-1/2">
                         <Image
-                            src="./images/bean-feed.jpg"
+                            priority
+                            src="/images/flyers/spring-doins-6030.jpg"
+                            alt="Spring Doin Flyer"
                             width={0}
                             height={0}
-                            alt="Bean Feed"
-                            className="w-full max-w-xl group-hover:opacity-5 mx-auto"
+                            className="w-full mx-auto"
                         />
                     </div>
                     <div className="basis-1/2">
                         <div className="flex flex-col basis-1/2 justify-center gap-4 mt-8 max-w-3xl mx-auto">
-                            <Heading>The 43rd Annual Bean Feed and Hawkers&apos; Faire</Heading>
-                            <div className="flex flex-col gap-1">
-                                <Paragraph>📅 Date: Feb 8, 6030 (2025)</Paragraph>
-                                <Paragraph>📍 Location: 1273 High Street, Auburn, CA </Paragraph>
-                                <Paragraph>💰 Cost: $35</Paragraph>
+                            <Heading>Lord Sholto Douglas Chapter 3 ECV Returns to Foresthill</Heading>
+                            <Subheading>Spring Doins 6030</Subheading>
+
+                            <div>
+                                <Paragraph>📅 Date: April 25-27, 2025 C.Y. 6030</Paragraph>
+                                <Paragraph>📍 Location: Foresthill, CA </Paragraph>
+                                <Paragraph>💰 Cost: TBD</Paragraph>
                             </div>
-
-                            <Paragraph>
-                                Brothers, gather &#39;round and prepare for an event so monumental, so flavorful, and so
-                                profoundly absurd that it could only come from the hallowed halls of the Lord Sholto
-                                Douglas #3 Chapter of E Clampus Vitus. This year&apos;s Great Bean Feed promises to be a
-                                culinary triumph, paying homage to the chapter&apos;s namesake while fortifying our
-                                spirits (and our bellies) with history, humor, and a healthy heap of beans.
-                            </Paragraph>
-
-                            <Paragraph>
-                                Under the benevolent leadership of Budman Also, Noble Grand Humbug Extraordinaire, the
-                                Lord Sholto Douglas #3 Chapter has outdone itself, crafting a celebration worthy of the
-                                greatest Clampers—and the hungriest jackasses—of all time.
-                            </Paragraph>
-
-                            <Paragraph xlarge bold>
-                                What Awaits:
-                            </Paragraph>
-                            <div className="flex flex-col gap-4 px-0 sm:px-8">
-                                <Paragraph bold>Beans Beyond Compare</Paragraph>
-                                <Paragraph>
-                                    Simmered to perfection in kettles the size of a miner&apos;s ambition, some of these
-                                    beans are said to be seasoned with a secret spice blend, whispered to Joe Zumwalt by
-                                    a ghostly prospector while visiting a forgotten Clamper plaque.
+                            <div>
+                                <Paragraph center>
+                                    LSD #3 Humbug &quot;Budman Also&quot; welcomes all brothers in good standing to 6030
+                                    Spring Doins!
                                 </Paragraph>
-
-                                <Paragraph bold>Epic Accompaniments</Paragraph>
-                                <Paragraph>
-                                    From slabs of cornbread thicker than a Clamper&apos;s wallet after payday to
-                                    sausages that have been declared historically significant in their own right, no
-                                    plate shall be left empty.
-                                </Paragraph>
-
-                                <Paragraph bold>Raucous Revelry</Paragraph>
-                                <Paragraph>
-                                    Expect tall tales, hearty laughter, and perhaps a spontaneous reenactment of the
-                                    Double Reverse Jackass Salute, should the crowd be deemed worthy.
+                                <Paragraph center>
+                                    3 Days of good food, full cup, and brotherhood at it&apos;s finest
                                 </Paragraph>
                             </div>
-                            <Paragraph>
-                                So grab your red shirts, polish your black hats, and prepare for an event that will go
-                                down in Clamper history as one of the finest displays of fraternal absurdity. Long live
-                                the Lord Sholto Douglas #3 Chapter, and long may our beans simmer!
-                            </Paragraph>
-
-                            <div className="flex justify-center gap-4 mt-8">
-                                <Paragraph>Payment accepted at the door.</Paragraph>
-                            </div>
+                            <Paragraph center>More information coming soon!</Paragraph>
                         </div>
                     </div>
                 </div>
             </PageSection>
+
+            {/*<PageSection heading="Upcoming Events" id="beanfeed">*/}
+            {/*    <div className="flex flex-col lg:flex-row gap-8 items-center justify-center">*/}
+            {/*        <div className="basis-1/2">*/}
+            {/*            <Image*/}
+            {/*                src="./images/bean-feed.jpg"*/}
+            {/*                width={0}*/}
+            {/*                height={0}*/}
+            {/*                alt="Bean Feed"*/}
+            {/*                className="w-full max-w-xl group-hover:opacity-5 mx-auto"*/}
+            {/*            />*/}
+            {/*        </div>*/}
+            {/*        <div className="basis-1/2">*/}
+            {/*            <div className="flex flex-col basis-1/2 justify-center gap-4 mt-8 max-w-3xl mx-auto">*/}
+            {/*                <Heading>The 43rd Annual Bean Feed and Hawkers&apos; Faire</Heading>*/}
+            {/*                <div className="flex flex-col gap-1">*/}
+            {/*                    <Paragraph>📅 Date: Feb 8, 6030 (2025)</Paragraph>*/}
+            {/*                    <Paragraph>📍 Location: 1273 High Street, Auburn, CA </Paragraph>*/}
+            {/*                    <Paragraph>💰 Cost: $35</Paragraph>*/}
+            {/*                </div>*/}
+
+            {/*                <Paragraph>*/}
+            {/*                    Brothers, gather &#39;round and prepare for an event so monumental, so flavorful, and so*/}
+            {/*                    profoundly absurd that it could only come from the hallowed halls of the Lord Sholto*/}
+            {/*                    Douglas #3 Chapter of E Clampus Vitus. This year&apos;s Great Bean Feed promises to be a*/}
+            {/*                    culinary triumph, paying homage to the chapter&apos;s namesake while fortifying our*/}
+            {/*                    spirits (and our bellies) with history, humor, and a healthy heap of beans.*/}
+            {/*                </Paragraph>*/}
+
+            {/*                <Paragraph>*/}
+            {/*                    Under the benevolent leadership of Budman Also, Noble Grand Humbug Extraordinaire, the*/}
+            {/*                    Lord Sholto Douglas #3 Chapter has outdone itself, crafting a celebration worthy of the*/}
+            {/*                    greatest Clampers—and the hungriest jackasses—of all time.*/}
+            {/*                </Paragraph>*/}
+
+            {/*                <Paragraph xlarge bold>*/}
+            {/*                    What Awaits:*/}
+            {/*                </Paragraph>*/}
+            {/*                <div className="flex flex-col gap-4 px-0 sm:px-8">*/}
+            {/*                    <Paragraph bold>Beans Beyond Compare</Paragraph>*/}
+            {/*                    <Paragraph>*/}
+            {/*                        Simmered to perfection in kettles the size of a miner&apos;s ambition, some of these*/}
+            {/*                        beans are said to be seasoned with a secret spice blend, whispered to Joe Zumwalt by*/}
+            {/*                        a ghostly prospector while visiting a forgotten Clamper plaque.*/}
+            {/*                    </Paragraph>*/}
+
+            {/*                    <Paragraph bold>Epic Accompaniments</Paragraph>*/}
+            {/*                    <Paragraph>*/}
+            {/*                        From slabs of cornbread thicker than a Clamper&apos;s wallet after payday to*/}
+            {/*                        sausages that have been declared historically significant in their own right, no*/}
+            {/*                        plate shall be left empty.*/}
+            {/*                    </Paragraph>*/}
+
+            {/*                    <Paragraph bold>Raucous Revelry</Paragraph>*/}
+            {/*                    <Paragraph>*/}
+            {/*                        Expect tall tales, hearty laughter, and perhaps a spontaneous reenactment of the*/}
+            {/*                        Double Reverse Jackass Salute, should the crowd be deemed worthy.*/}
+            {/*                    </Paragraph>*/}
+            {/*                </div>*/}
+            {/*                <Paragraph>*/}
+            {/*                    So grab your red shirts, polish your black hats, and prepare for an event that will go*/}
+            {/*                    down in Clamper history as one of the finest displays of fraternal absurdity. Long live*/}
+            {/*                    the Lord Sholto Douglas #3 Chapter, and long may our beans simmer!*/}
+            {/*                </Paragraph>*/}
+
+            {/*                <div className="flex justify-center gap-4 mt-8">*/}
+            {/*                    <Paragraph>Payment accepted at the door.</Paragraph>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</PageSection>*/}
 
             {/*<PageSection heading="Quick Links" id="quick-links">*/}
             {/*    <div className="flex justify-center gap-4 mt-8">*/}
